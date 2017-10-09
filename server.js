383c77998d2d9bef77ed630b17e11ff87ae87fce
Pasquale <sorrentino-pasquale@hotmail.it>
2017-10-09 21:48:27 +0200

    Changin port, fixed to 3000 or user choice

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT; // Don't use alternative port, because some projects will point to it
 
 
 const setRoutes = require('./routes/generalRoutes'); //importing route
 setRoutes(app, port); //register the route
-
 
 app.listen(port);
 console.log('fake-JSON-backend server started on: ' + port);
